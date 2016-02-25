46357761f0c2fc8c24470d17e84f58d5a1fecf34
stevelyons <steve@stevelyons.com>
2016-02-24 23:35:54 -0600

    Using NSLog

---
+++ prog1/prog1/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  prog1
+//
+//  Created by Steve Lyons on 2/24/16.
+//  Copyright © 2016 Lyons Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool
+    {
+        NSLog(@"Programming is fun!");
+        NSLog(@"Programming in Objective-C is even more fun!");
+    }
+    return 0;
+}
